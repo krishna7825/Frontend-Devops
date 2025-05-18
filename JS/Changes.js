87167c472fd2400533889f452917533bf20f3e84
krishna <168535618+krishna7825@users.noreply.github.com>
2025-05-19 01:02:14 +0530

    First Commit

---
+++ JS/Changes.js
@@ -0,0 +1,7 @@
+let gameName = "Chess"; 
+gameName = "Checkers";
+console.log("Game name is: " + gameName); // Output: Game name is: Checkers
+const gameType = "Board";
+// gameType = "Card"; // This will throw an error because const variables cannot be reassigned  
+console.log("Game type is: " + gameType); // Output: Game type is: Board
+
