c5382300f819b17c4d49ac85a9641c7bba383973
krishna <168535618+krishna7825@users.noreply.github.com>
2025-05-19 01:00:43 +0530

    First Commit

---
+++ JS/Operators.js
@@ -0,0 +1,68 @@
+// let addition = 4+5;
+// let score = 102;
+// let bonus = 10;
+// let totalScore = score + bonus;
+
+// let subtraction = 10-5;
+// console.log("Subtraction: " + subtraction); // Output: Subtraction: 5
+// let difference = 10-5;
+// console.log("Difference: " + difference); // Output: Difference: 5
+// let product = 10*5;
+// console.log("Product: " + product); // Output: Product: 50
+
+// let division = 10/5;
+// console.log("Division: " + division); // Output: Division: 2
+// let modulus = 10%3;
+// console.log("Modulus: " + modulus); // Output: Modulus: 1
+// let exponentiation = 2**3;
+// console.log("Exponentiation: " + exponentiation); // Output: Exponentiation: 8
+// let increment = 5;
+// increment++;
+// console.log("Increment: " + increment); // Output: Increment: 6
+// let decrement = 5;
+// decrement--;
+// console.log("Decrement: " + decrement); // Output: Decrement: 4
+// let assignment = 10;
+// assignment += 5;
+// console.log("Assignment: " + assignment); // Output: Assignment: 15
+
+
+
+// Comparison Operators
+// let a = 5;
+// let b = 10;
+// let isEqual = a == b;
+// console.log("Is Equal: " + isEqual); // Output: Is Equal: false
+
+// let isNotEqual = a != b;
+// console.log("Is Not Equal: " + isNotEqual); // Output: Is Not Equal: true
+// let isStrictEqual = a === b;
+// console.log("Is Strict Equal: " + isStrictEqual); // Output: Is Strict Equal: false
+// let isStrictNotEqual = a !== b;
+// console.log("Is Strict Not Equal: " + isStrictNotEqual); // Output: Is Strict Not Equal: true 
+
+
+// // Logical Operators
+// let x = true;
+// let y = false;
+// let andOperator = x && y;
+// console.log("And Operator: " + andOperator); // Output: And Operator: false
+// let orOperator = x || y;
+
+// console.log("Or Operator: " + orOperator); // Output: Or Operator: true
+// let notOperator = !x;
+// console.log("Not Operator: " + notOperator); // Output: Not Operator: false
+
+// Bitwise Operators
+let bitwiseAnd = 5 & 3;
+console.log("Bitwise And: " + bitwiseAnd); // Output: Bitwise And: 1
+let bitwiseOr = 5 | 3;
+console.log("Bitwise Or: " + bitwiseOr); // Output: Bitwise Or: 7
+let bitwiseXor = 5 ^ 3;
+console.log("Bitwise Xor: " + bitwiseXor); // Output: Bitwise Xor: 6
+let bitwiseNot = ~5;
+console.log("Bitwise Not: " + bitwiseNot); // Output: Bitwise Not: -6
+let leftShift = 5 << 1;
+console.log("Left Shift: " + leftShift); // Output: Left Shift: 10
+let rightShift = 5 >> 1;
+console.log("Right Shift: " + rightShift); // Output: Right Shift: 2
